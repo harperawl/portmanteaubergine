20ef4171e6ab05b951a811aa1a79802eb9ddc884
SpikyLlama <spikyllamaaa@gmail.com>
2024-10-22 11:15:20 -0700

    new word list

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 async function fetchWordList() {
     try {
-        let response = await fetch('3of6game.txt');
-        let data = (await response.text()).split('\n');
+        let response = await fetch('sowpods.txt');
+        let data = (await response.text()).split('\r\n');
         return data;
     } catch (error) {
         console.error('Error fetching the random word:', error);
